5984450e0ac9188bc8e28e47d5b73ca2d49eaf83
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-31 19:16:54 +0530

    [Afzal] Added: FlipCoin Class

---
+++ CoreProgram1/CoreProgram1/FlipCoin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreProgram1
+{
+    public class FlipCoin
+    {
+        public void play(float num)
+        {
+            Random random = new Random();
+            float head = 0;
+            for(int i = 0; i < num; i++)
+            {
+               int  coin = random.Next(1,3);
+                if(coin == 1)
+                {
+                    head += 1;
+                }
+            }
+            float percent = head / num;
+            Console.WriteLine(percent);
+        }
+    }
+}
--- CoreProgram1/CoreProgram1/Program.cs
+++ CoreProgram1/CoreProgram1/Program.cs
@@ -7,6 +7,16 @@ namespace CoreProgram1
         static void Main(string[] args)
         {
             Console.WriteLine("CoreProgram Exercise 1");
+            Console.WriteLine("1.FlipCoin" + "\n");
+            Console.WriteLine("Enter option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.play(100);
+                    break;
+            }
         }
     }
 }
