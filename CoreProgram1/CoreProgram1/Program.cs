55d6e88679f4ecc3338601d56898c649196f576f
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-31 20:19:09 +0530

    [Afzal] Added: LeapYear

--- CoreProgram1/CoreProgram1/Program.cs
+++ CoreProgram1/CoreProgram1/Program.cs
@@ -7,7 +7,8 @@ namespace CoreProgram1
         static void Main(string[] args)
         {
             Console.WriteLine("CoreProgram Exercise 1");
-            Console.WriteLine("1.FlipCoin" + "\n");
+            Console.WriteLine("1.FlipCoin" + "\n" +
+                                "2.LeapYear" + "\n");
             Console.WriteLine("Enter option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -15,6 +16,10 @@ namespace CoreProgram1
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.play(100);
+                    break;
+                case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.Year();
                     break;
             }
         }
