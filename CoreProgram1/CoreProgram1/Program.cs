a010e629595bf11e4d1eb38fec37f52694910758
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-31 17:07:06 +0530

    [Afzal] Add: Folder Structure

---
+++ CoreProgram1/CoreProgram1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreProgram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CoreProgram Exercise 1");
+        }
+    }
+}
