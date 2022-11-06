77416bd3df47f004187e7dcf0649e552e9cafc50
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-06 22:15:18 +0530

    [Afzal] Added : PowerOfTwo

---
+++ CoreProgram1/CoreProgram1/PowerofTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreProgram1
+{
+    public class PowerofTwo
+    {
+        public void Power(int num)
+        {
+            for(int i=0; i <= num; i++)
+            {
+                Console.WriteLine("2^"+i+" = "+Math.Pow(2,i));
+            }
+        }    
+    }
+}
--- CoreProgram1/CoreProgram1/Program.cs
+++ CoreProgram1/CoreProgram1/Program.cs
@@ -8,7 +8,8 @@ namespace CoreProgram1
         {
             Console.WriteLine("CoreProgram Exercise 1");
             Console.WriteLine("1.FlipCoin" + "\n" +
-                                "2.LeapYear" + "\n");
+                                "2.LeapYear" + "\n" +
+                                "3.Powerof2"+"\n");
             Console.WriteLine("Enter option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -20,6 +21,12 @@ namespace CoreProgram1
                 case 2:
                     LeapYear leapYear = new LeapYear();
                     leapYear.Year();
+                    break;
+                case 3:
+                    PowerofTwo powerofTwo = new PowerofTwo();
+                    Console.WriteLine("\n"+"Enter the power value");
+                    int power = Convert.ToInt32(Console.ReadLine());
+                    powerofTwo.Power(power);
                     break;
             }
         }
