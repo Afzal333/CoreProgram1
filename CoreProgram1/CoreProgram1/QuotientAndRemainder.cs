c0f30d23b0883e23ee6fbf77d0c2b0cd3f5eca66
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-06 23:27:27 +0530

    [Afzal] Added: Quotient and Remainder

---
+++ CoreProgram1/CoreProgram1/QuotientAndRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreProgram1
+{
+    public class QuotientAndRemainder
+    {
+        public void DivRem()
+        {
+            Console.WriteLine("Enter the dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+
+            int quotient = Math.DivRem(dividend,divisor,out result);
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("Remainder is " + result);
+        }
+    }
+}
